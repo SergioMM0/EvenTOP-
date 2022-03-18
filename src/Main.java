8fdf1e427a5e2ed314c690976860d381aee676fb
Sergio M <serg0971@easv365.dk>
2022-03-18 09:56:42 +0100

    Creating LogIn

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui/view/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui/view/MainLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/gui/controller/MainLogController.java
+++ src/gui/controller/MainLogController.java
@@ -3,7 +3,7 @@ package gui.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class LoginController {
+public class MainLogController {
     @FXML
     private Label welcomeText;
 
