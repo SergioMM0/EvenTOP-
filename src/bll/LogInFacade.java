d33161e650480c9c49b2e31c6756e0843ea257ff
Sergio M <serg0971@easv365.dk>
2022-03-18 11:00:16 +0100

    switch login button working

---
+++ src/bll/LogInFacade.java
@@ -0,0 +1,7 @@
+package bll;
+
+public interface LogInFacade {
+
+    boolean checkCredentials(String email, String password);
+
+}
--- src/gui/controller/MainLogController.java
+++ src/gui/controller/MainLogController.java
@@ -1,12 +1,19 @@
 package gui.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class MainLogController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainLogController implements Initializable {
+
+    private boolean isEM = false;
 
     @FXML
     private TextField emailField;
@@ -15,12 +22,27 @@ public class MainLogController {
     private ImageView frontImage;
 
     @FXML
-    private JFXButton loginButton;
-
-    @FXML
     private PasswordField passwordField;
 
     @FXML
-    private JFXButton switchLogin;
+    private JFXButton switchLoginButton;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchLoginAct(ActionEvent event) {
+        isEM = !isEM;
+        if (isEM){
+            switchLoginButton.setText("I'm a customer");
+        }
+        else switchLoginButton.setText("I'm an Event Manager");
+    }
 }
