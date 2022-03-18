e78ddb0292019449fb7eddfb23dcef220c27f2e5
Sergio M <serg0971@easv365.dk>
2022-03-18 10:38:17 +0100

    controller basic setup

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui/view/MainLogin.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 450);
+        stage.setResizable(false);
+        stage.setTitle("EvenTOP");
         stage.setScene(scene);
         stage.show();
     }
--- src/gui/controller/MainLogController.java
+++ src/gui/controller/MainLogController.java
@@ -1,14 +1,26 @@
 package gui.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class MainLogController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField emailField;
+
+    @FXML
+    private ImageView frontImage;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private JFXButton switchLogin;
+
 }
