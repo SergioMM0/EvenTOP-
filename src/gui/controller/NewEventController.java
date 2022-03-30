f0546376a3594d35e89395fc6e320aabdf03abe7
Sergio M <serg0971@easv365.dk>
2022-03-30 10:01:51 +0200

    new Event view

---
+++ src/gui/controller/NewEventController.java
@@ -0,0 +1,14 @@
+package gui.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewEventController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
